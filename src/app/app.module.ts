6b76f41fbfcca521895f371766a27d9cdedcf509
Emertha <36954414+Emertha@users.noreply.github.com>
2019-09-15 03:27:50 -0700

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InvoicingModule } from './invoicing/invoicing.module';
+import { HeroModule } from './hero/hero.module';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { ContactComponent } from './contact//contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    InvoicingModule,
+    HeroModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
